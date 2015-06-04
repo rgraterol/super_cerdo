5600e9483a21357543492ef57444325f13d1317b
xavier <rgraterol@biosoftca.com>
2015-06-04 18:15:33 -0430

    [DONE] INDEX RESPONSIVE PARA DESKTOP

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,12 +1,25 @@
 @extends('app')
 
 @section('content')
-<div class="container-fluid no-padding">
+<div class="container-fluid no-padding" style="position: relative;                                                      ">
     <div class="row">
-        <div class="col-xs-5" id="banda">
+        <div class="col-xs-5 col-sm-5 col-md-5 col-lg-5 banda">
 
+        </div>
+    </div>
+    <div class="row footer">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 costillas">
+            <ul>
+                <li><img src="img/producto-1-normal.png" class="img-responsive inline-block"></li>
+                <li><img src="img/producto-2-normal.png" class="img-responsive inline-block"></li>
+                <li><img src="img/producto-3-normal.png" class="img-responsive inline-block"></li>
+                <li><img src="img/producto-4-normal.png" class="img-responsive inline-block"></li>
+                <li><img src="img/producto-5-normal.png" class="img-responsive inline-block"></li>
+                <li><img src="img/producto-6-normal.png" class="img-responsive inline-block"></li>
+                <li><img src="img/producto-7-normal.png" class="img-responsive inline-block"></li>
+            </ul>
         </div>
     </div>
 </div>
 
-@endsection
+@endsection             
