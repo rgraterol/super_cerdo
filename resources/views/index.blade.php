97dbf970000cd9f10af00f15dc85b6322589d87f
xavier <rgraterol@biosoftca.com>
2015-06-09 19:39:29 -0430

    -WORKING- CSS

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -205,9 +205,7 @@
             <div class="row footer">
                 <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 costillas">
                     <ul>
-                        <li>
-                            <div class="detalle-bbq"></div>
-                        </li>
+                        <li><a href="img/lightbox/1-DETALLE-BARBECUE.png" title="" data-gallery="" ><img src="img/carousel_big/1-costillita-barbecue-on.png" class="img-responsive inline-block"></a></li>
                         <li><a href="img/lightbox/2-DETALLE-CHILENA.png" title="" data-gallery="" ><img src="img/carousel_big/2-costillitas-chilenas-on.png" class="img-responsive inline-block"></a></li>
                         <li><a href="img/lightbox/3-DETALLE-IBERICA.png" title="" data-gallery="" ><img src="img/carousel_big/3-costillitas-ibericas-on.png" class="img-responsive inline-block"></a></li>
                         <li><a href="img/lightbox/4-DETALLE-MEXICANA.png" title="" data-gallery="" ><img src="img/carousel_big/4-costillitas-mexicanas-on.png" class="img-responsive inline-block"></a></li>
