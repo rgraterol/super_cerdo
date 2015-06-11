bbedfd9f10912705cb3a1901633ec4a77e0e18a1
xavier <rgraterol@biosoftca.com>
2015-06-11 16:37:04 -0430

    UNICORNIO CON VIDEO

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -79,8 +79,23 @@
 
     </script>
     <script type="text/javascript">
-        console.log(document.referrer);
-        if(document.referrer == "www.youtube.com"){
+        function getSearchParameters() {
+            var prmstr = window.location.search.substr(1);
+            return prmstr != null && prmstr != "" ? transformToAssocArray(prmstr) : {};
+        }
+
+        function transformToAssocArray( prmstr ) {
+            var params = {};
+            var prmarr = prmstr.split("&");
+            for ( var i = 0; i < prmarr.length; i++) {
+                var tmparr = prmarr[i].split("=");
+                params[tmparr[0]] = tmparr[1];
+            }
+            return params;
+        }
+        var params = getSearchParameters();
+        console.log(params.href);
+        if(params.href == "youtube"){
 
         }else{
             $(function () {
