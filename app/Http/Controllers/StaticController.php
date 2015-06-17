1996b64ce88f48c6544aaa07a26912efadda9944
Xavier Graterol <xavier.graterol@gmail.com>
2015-06-17 10:39:11 -0430

    dwa

--- app/Http/Controllers/StaticController.php
+++ app/Http/Controllers/StaticController.php
@@ -29,7 +29,7 @@ class StaticController extends Controller {
         {
             return response()->json(['false']);
         }else{
-            echo $data->birthday;
+            $data->birthday = date("Y-m-d", strtotime($data->birthday));
             $client = new User($data);
             $client->save();
             return response()->json($client);
